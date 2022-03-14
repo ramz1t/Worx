940c98e035e9e6b72fdfb96b02cc6c4d0742c585
Timur Ramazanov <timurram007@gmail.com>
2022-03-14 13:29:24 +0300

    added class AuthSession

--- models/auth.py
+++ models/auth.py
@@ -1,6 +1,14 @@
 from pydantic import BaseModel
+from sqlalchemy import Column, String, Integer
+from data.data import Base
 
 
 class ApiNewAuth(BaseModel):
     email: str
     password: str
+
+
+class AuthSession(Base):
+    __tablename__ = 'AuthSessions'
+    token = Column(String, primary_key=True)
+    user_id = Column(Integer)
