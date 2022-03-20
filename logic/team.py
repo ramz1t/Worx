5daac6bf127695a40bd31d1bb4d2aa77d455d64f
Timur Ramazanov <timurram007@gmail.com>
2022-03-20 18:58:28 +0300

    added change_team_url func

--- logic/team.py
+++ logic/team.py
@@ -9,10 +9,20 @@ def get_team_by_id(team_id: int) -> Team or None:
 
 def change_team_name(name: str, id: int) -> bool:
     with Sessions() as session:
-        team = get_team_by_id()
+        team = get_team_by_id(id)
         if team is None:
             return False
         team.name = name
         session.add(team)
         session.commit()
     return True
+
+
+def change_team_url(url: str, id: int) -> bool:
+    with Sessions() as session:
+        team = get_team_by_id(id)
+        if team is None:
+            return False
+        team.url = url
+        session.add(team)
+        session.commit()
