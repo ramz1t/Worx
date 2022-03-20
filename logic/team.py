f8381f88333b930c62138524ff7a426ed2ee0775
Timur Ramazanov <timurram007@gmail.com>
2022-03-20 19:24:03 +0300

    added delete from db funcs for teams

--- logic/team.py
+++ logic/team.py
@@ -7,6 +7,10 @@ def get_team_by_id(team_id: int) -> Team or None:
         return session.query(Team).filter_by(id=team_id).first()
 
 
+def get_team_by_name(team_name: str) -> Team or None:
+    with Sessions() as session:
+        return session.query(Team).filter_by(name=team_name)
+
 def change_team_name(name: str, id: int) -> bool:
     with Sessions() as session:
         team = get_team_by_id(id)
@@ -30,3 +34,15 @@ def change_team_url(url: str, id: int) -> bool:
 
 def create_team(team: ApiCreateTeam):
     return Team(name=team.name, url=team.url)
+
+
+def delete_team_by_id(team_id: int):
+    with Sessions() as session:
+        session.query(Team).filter_by(id=team_id).delete()
+        session.commit()
+
+
+def delete_team_by_name(team_name: int):
+    with Sessions() as session:
+        session.query(Team).filter_by(id=team_name).delete()
+        session.commit()
