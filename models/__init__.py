be900b5cf801f2c129fb175db836c6720beebd05
Рамазанов Тимур <timurram007@gmail.com>
2022-04-04 09:39:11 +0000

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,13 +1,1 @@
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.future import create_engine
-from sqlalchemy.orm import sessionmaker
-import models.data as base
-
-base.Base = declarative_base()
-
-from models import *
-
-base.Engine = create_engine('sqlite:///data/data.db')
-base.Base.metadata.create_all(base.Engine)
-base.Sessions = sessionmaker(bind=base.Engine)
 
