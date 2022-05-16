7ffeabfa02ed91de4c684222ce6204550e04d0f0
Timur Ramazanov <timurram007@gmail.com>
2022-05-16 22:31:22 +0300

    добавил пару ручек, теперь нужно сделать бд с именем вледельца.

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 from fastapi import FastAPI, requests, status
 from fastapi.responses import JSONResponse
 import uvicorn
-from git.gitfuncs import get_repo_commits, get_repo_contributors
+from git.gitfuncs import get_repo_commits, get_repo_contributors, get_user_repo_stats
 from git.params import Auth_params
+import json
+
 
 app = FastAPI()
 
 
-@app.get('/repo/{repourl}/{username}')
-def get_repo_stats(repourl, username):
-    commits = get_repo_contributors(repo_name=repourl, auth_params=Auth_params, users_name=username)
-    for i in commits:
-        if i['login'] == username:
-            return i
+@app.get('/commits/{reponame}/{username}')
+def get_repo_user_commits(reponame, username):
+    commits = get_repo_contributors(repo_name=reponame, auth_params=Auth_params, users_name=username)
+    return commits
+
+
+@app.get('/stats/{reponame}')
+def get_repo_stats(reponame):
+    username = 'AlexGyver'
+    stats = get_user_repo_stats(repo_name=reponame, auth_params=Auth_params, users_name=username)
+    result = {'description': stats['description'], 'link': 'https://github.com/{}/{}'.format(username, reponame)}
+    return result
 
 
 if __name__ == "__main__":  # Запуск сервера
