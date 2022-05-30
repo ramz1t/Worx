0b869402436d39e235935a9a6a8c6cde171585ce
Timur Ramazanov <timurram007@gmail.com>
2022-05-30 23:59:05 +0300

    added auth to account

--- views/static/js/login.js
+++ views/static/js/login.js
@@ -1,6 +1,16 @@
 async function login() {
     var pass = document.getElementById("password").value;
     var email = document.getElementById("email").value;
-    var response = await fetch(SERVER_DOMAIN + '/token');
+    var data = {'username': email, 'password': pass};
+    var response = await fetch(SERVER_DOMAIN + '/token', {
+        method: 'POST',
+        headers: {
+            'accept': 'application/json'
+        },
+        body: new URLSearchParams({
+            'username': email,
+            'password': pass
+        })
+    });
     console.log(response);
 }
