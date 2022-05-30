757786fd59290a299188b1c6d9199226e1973bad
Timur Ramazanov <timurram007@gmail.com>
2022-05-30 23:03:35 +0300

    fix typo

--- logic/user.py
+++ logic/user.py
@@ -4,10 +4,11 @@ from models.user import User, ApiCreateUser
 from data.data import Sessions
 from logic.auth import get_password_hash
 
+
 def create_new_user(ApiUser) -> JSONResponse:
     with Sessions() as session:
         if not session.query(User).filter_by(email=ApiUser.email).first() is None:
-            return JSONResponse(status_code=status.HTTP_409_CONFLICT, content='Username already in use')
+            return JSONResponse(status_code=status.HTTP_409_CONFLICT, content='Email already in use')
         if len(ApiUser.password) < 8 or len(ApiUser.email) < 4:
             return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content='Invalid data')
         user = User(email=ApiUser.email, password=get_password_hash(ApiUser.password), gender=ApiUser.gender)
@@ -21,6 +22,7 @@ def change_user(_id, new_email):
         user = session.query(User).filter_by(id=_id).first()
         user.email = new_email
         return JSONResponse(status_code=status.HTTP_201_CREATED, content='Successfully')
+
 
 def get_user_info(user_id: int):
     with Sessions() as session:
